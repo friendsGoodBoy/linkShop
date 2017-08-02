f5390665f4f9e030d2737111e2e290f5b4747357
linkzz <linkzz@foxmail.com>
2017-08-02 18:23:59 +0800

    爬虫程序优化

--- webMagic/src/main/java/com/link/webMagic/GithubRepoPageProcessor.java
+++ webMagic/src/main/java/com/link/webMagic/GithubRepoPageProcessor.java
@@ -3,23 +3,24 @@ package com.link.webMagic;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.AfterExtractor;
 import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-public class GithubRepoPageProcessor implements PageProcessor {
+public class GithubRepoPageProcessor implements PageProcessor,AfterExtractor {
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000);
 
     @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("(http://www\\.gslzcredit\\.gov\\.cn/[\\w\\-]+/[\\w\\-]+)").all());
-        page.addTargetRequests(page.getHtml().links().regex("(http://www\\.gslzcredit\\.gov\\.cn/[\\w\\-])").all());
+        page.addTargetRequests(page.getHtml().links().regex("(http://www\\.gslzcredit\\.gov\\.cn/[\\w\\-]+/[\\w\\-]+\\.html)").all());
+        page.addTargetRequests(page.getHtml().links().regex("(http://www\\.gslzcredit\\.gov\\.cn/[\\w\\-]+\\.html)").all());
         //page.putField("author", page.getUrl().regex("http://www\\.gslzcredit\\.gov\\.cn/(\\w+)/.*").toString());
         page.putField("title", page.getHtml().xpath("//p[@class='MsoPlainText']/span/font/text()").toString());
         if (page.getResultItems().get("title")==null){
             //skip this page
             page.setSkip(true);
         }
-        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+        page.putField("name", page.getHtml().xpath("//table[@class='MsoNormalTable']/tidyText()"));
     }
 
     @Override
@@ -27,7 +28,15 @@ public class GithubRepoPageProcessor implements PageProcessor {
         return site;
     }
 
+    @Override
+    public void afterProcess(Page page) {
+
+    }
+
     public static void main(String[] args) {
+
+
         Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.gslzcredit.gov.cn/65/").addPipeline(new JsonFilePipeline("D:\\data\\webmagic")).thread(5).run();
     }
+
 }
